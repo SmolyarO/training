0479ae1ba6492b72074e24650003cd3ac352b652
Ostap Smolyar <ostap.smolyar@gmail.com>
2015-01-05 23:58:11 +0200

    js-function in progress

--- js-function/cache.js
+++ js-function/cache.js
@@ -1,5 +1,78 @@
-window.onload = function() {
-	function Cache() {
-		
-	}
+function Cache() {
+    if (!(this instanceof Cache)) {
+        return new Cache();
+    }
+
+    var kvs = {},
+        add,
+        get,
+        update,
+        del,
+        find;
+
+    add = function(key, value) {
+        if (!(key in kvs)) {
+            kvs[key] = value;
+        }
+        else {
+            console.log('Duplicate key');
+        }
+
+    }
+
+    update = function(key, value) {
+        if ((key in kvs)) {
+            kvs[key] = value;
+        }
+        else {
+            add(key, value);
+        }
+
+    }
+
+    del = function(key) {
+        if ((key in kvs)) {
+            delete kvs[key];
+        }
+        else {
+            console.log('No such key');
+        }
+
+    }
+
+
+    get = function(key) {
+        if ((key in kvs)) {
+            return kvs[key];
+        }
+        else {
+            console.log('No such key');
+        }
+
+    }
+
+    find = function(str) {
+        if (!(key in kvs)) {
+            kvs[key] = value;
+        }
+        else {
+            console.log('Not found');
+        }
+
+    }
+
+
+    return {
+        kvs: kvs,
+        add: add,
+        update: update,
+        del: del,
+        get: get,
+        find: find
+    }
 }
+
+var c = new Cache();
+c.add('test', 21);
+c.add('test', 22);
+c.del('test');
