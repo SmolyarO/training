afd43cda393dcfddfddaaffc9249e023074eb3f1
Ostap Smolyar <ostap.smolyar@gmail.com>
2015-01-06 15:13:47 +0200

    js-function done

--- js-function/cache.js
+++ js-function/cache.js
@@ -8,58 +8,97 @@ function Cache() {
         get,
         update,
         del,
-        find;
+        find,
+        count;
 
     add = function(key, value) {
-        if (!(key in kvs)) {
+        if (arguments.length < 2) {
+            console.log('Key or Value is absent');
+        }
+        else if (!(key in kvs)) {
             kvs[key] = value;
         }
         else {
             console.log('Duplicate key');
         }
 
-    }
+    };
 
     update = function(key, value) {
-        if ((key in kvs)) {
+        if (arguments.length < 2) {
+            console.log('Key or Value is absent');
+        }
+        else if ((key in kvs)) {
             kvs[key] = value;
         }
         else {
             add(key, value);
         }
 
-    }
+    };
 
     del = function(key) {
-        if ((key in kvs)) {
+        if (!key) {
+            console.log('Key is absent');
+        }
+        else if ((key in kvs)) {
             delete kvs[key];
         }
         else {
             console.log('No such key');
         }
 
-    }
+    };
 
 
     get = function(key) {
-        if ((key in kvs)) {
+        if (!key) {
+            console.log('Key is absent');
+        }
+        else if (key in kvs) {
             return kvs[key];
         }
         else {
             console.log('No such key');
         }
 
-    }
+    };
 
-    find = function(str) {
-        if (!(key in kvs)) {
-            kvs[key] = value;
+    find = function(query) {
+        if (!query) {
+            console.log('There is no query')
+        }
+        else if (query) {
+            var result = [],
+                prop;
+
+            for (prop in kvs) {
+                if (kvs.hasOwnProperty(prop) && (kvs[prop].toString().indexOf(query)) != -1) {
+                    var obj = {};
+                    obj[prop] = kvs[prop];
+                    result.push(obj);
+                }
+            }
+
+            console.log(result);
+
+            if (result.length == 0) {
+                console.log('Sorry, I have found nothing :(');
+            }
+        }
+
+    };
+
+    count = function(key) {
+        if (key && key in kvs) {
+            console.log(kvs[key].length);
+            //maybe better use switch
         }
         else {
-            console.log('Not found');
+            console.log('Total cache prop count: ' + Object.keys(kvs).length);
         }
 
-    }
+    };
 
 
     return {
@@ -68,11 +107,21 @@ function Cache() {
         update: update,
         del: del,
         get: get,
-        find: find
+        find: find,
+        count: count
+    }
+}
+
+//trash generator
+function trash (obj, count) {
+    for (var i = 0; i < count; i++) {
+        obj.add([Math.floor((Math.random() * 10000) + 1)],  Math.floor((Math.random() * 10000) + 1));
     }
 }
 
 var c = new Cache();
 c.add('test', 21);
-c.add('test', 22);
+c.add('test', 22);  // duplicate
+c.add('test2', 'test string');
+trash(c, 20);
 c.del('test');
