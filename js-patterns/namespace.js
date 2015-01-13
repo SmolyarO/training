3f31e0a6e53fbdf17f7d8bee56dee391af2b5536
Ostap Smolyar <ostap.smolyar@gmail.com>
2015-01-13 13:44:37 +0200

    namespace in progress

---
+++ js-patterns/namespace.js
@@ -0,0 +1,42 @@
+// AVID namespace task
+var AVID = AVID || {};
+
+AVID.namespace = function (nsString) {
+    var parts = nsString.split('.'),
+        parent = AVID,
+        i;
+
+    if (parts[0] === "AVID") {
+        parts = parts.slice(1);
+    }
+
+    for (i = 0; i < parts.length; i += 1) {
+        if (typeof parent[parts[i]] === "undefined") {
+            parent[parts[i]] = {};
+        }
+        parent = parent[parts[i]];
+    }
+    return parent;
+
+};
+
+var assets,
+    users,
+    links,
+    parser,
+    checksum,
+    rest,
+    bus;
+
+// models
+assets = AVID.namespace('models.assets');
+users = AVID.namespace('models.users');
+links = AVID.namespace('models.links');
+
+// helpers
+parser = AVID.namespace('helpers.parser');
+checksum = AVID.namespace('helpers.checksum');
+
+// apis
+rest = AVID.namespace('apis.rest');
+bus = AVID.namespace('apis.bus');
